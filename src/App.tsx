0fbe372f298c3714a4cfe95d1af832a90d65d5e7
Rone <rone_sigismundo@hotmail.com>
2021-03-16 21:19:36 -0300

    Finish application layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { api } from "./services/api";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function HandleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function HandleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
+  useEffect(()=>{
+    api.get('transactions')
+      .then(response => console.log(response.data))
+  },[]);
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={HandleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={HandleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
